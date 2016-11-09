6c8275f7852d4b4a28af4d754f63c47ba36ca1e1
Rodrigo Calderon <rcalderon@simons.com.py>
2016-11-09 01:03:31 -0300

    Formulario VentasSin

--- src/ContabilidadBundle/Form/VentaSinType.php
+++ src/ContabilidadBundle/Form/VentaSinType.php
@@ -4,6 +4,7 @@ namespace ContabilidadBundle\Form;
 
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,33 +21,48 @@ class VentaSinType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('fecha', DateType::class, array(
-                'format'=>'dd-MM-yyyy',
-                'html5'=>true
+            ->add('fecha', BirthdayType::class, array(
+                'widget'=>'single_text',
+                'input'=>'datetime',
+                'data'=>new \DateTime()
+
             ))
             ->add('comprobante')
             ->add('cotiz')
             ->add('comentario')
             ->add('g10', IntegerType::class, array(
-                'data'=>0
+                'data'=>0,
+                'attr'=>array('class'=>'text-right')
             ))
             ->add('g5', IntegerType::class, array(
-                'data'=>0
+                'data'=>0,
+                'attr'=>array('class'=>'text-right')
             ))
             ->add('iva10', IntegerType::class, array(
-                'data'=>0
+                'data'=>0,
+                'attr'=>array('class'=>'text-right')
             ))
             ->add('iva5', IntegerType::class, array(
-                'data'=>0
+                'data'=>0,
+                'attr'=>array('class'=>'text-right')
             ))
             ->add('exe', IntegerType::class, array(
-                'data'=>0
+                'data'=>0,
+                'attr'=>array('class'=>'text-right')
             ))
             ->add('retencion', IntegerType::class, array(
-                'data'=>0
+                'data'=>0,
+                'attr'=>array('class'=>'text-right')
             ))
             ->add('timbrado')
-            ->add('condicion')
+            ->add('condicion', ChoiceType::class, array(
+                'choices'=>array(
+                    'Contado'=>0,
+                    'Credito'=>1
+                ),
+                'expanded'=>false,
+                'placeholder'=>false
+            ))
             ->add('sucursal')
             ->add('entidad')
             ->add('cliente')
